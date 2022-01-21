8e282bdb7b4d66972c5c4f1c29427b55ef52d3b3
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-21 14:27:22 -0300

    feat: environment db conection and server port

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -29,6 +29,8 @@ app.use(
   },
 );
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}!`);
 });
