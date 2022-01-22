eedbaf0fcaf1911c248573411d783ec4413e90fa
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-22 00:50:15 -0300

    fix: errorHandler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction, response } from 'express';
 import 'reflect-metadata';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import { AppError } from '../errors/appError';
@@ -12,20 +13,18 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (error: Error, request: Request, reponse: Response, next: NextFunction) => {
-    {
-      if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-          status: 'error',
-          message: error.message,
-        });
-      }
-
-      return response.status(500).json({
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
         status: 'error',
-        message: 'Internal server Error',
+        message: error.message,
       });
     }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server Error',
+    });
   },
 );
 
