73f3397b90e3b464ee2025d64654d32d0743091b
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-22 00:07:39 -0300

    feat: gitinore

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import UserRoute from '../../../modules/users/routes/UserRoute';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ mensage: 'Hello Caio' });
 });
+
+routes.use('/user', UserRoute);
 
 export default routes;
