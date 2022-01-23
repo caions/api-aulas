759e586f82e04f40175445b497bd886102a2d740
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-23 14:17:54 -0300

    refactor: create middleware handlerError

---
+++ src/shared/middlewares/handlerError.ts
@@ -0,0 +1,21 @@
+import { AppError } from '@shared/errors/appError';
+import { NextFunction, Request, Response } from 'express';
+
+export const HandlerError = (
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  console.log(error);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server Error',
+  });
+};
