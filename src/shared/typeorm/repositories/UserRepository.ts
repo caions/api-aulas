1f5ee3e94c75cdc95247e535fd754c289cb352d6
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-23 12:22:15 -0300

    feat: UserRepository implements Inteface

--- src/shared/typeorm/repositories/UserRepository.ts
+++ src/shared/typeorm/repositories/UserRepository.ts
@@ -1,9 +1,36 @@
+import { IUserRepository } from '@modules/users/interfaces/IUserRepository';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../entities/users';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {
-  public async findByEmail(email: string): Promise<User | undefined> {
+export class UserRepository
+  extends Repository<User>
+  implements IUserRepository
+{
+  async index() {
+    return await this.find();
+  }
+
+  async findById(id: string) {
+    return await this.findOne({ id });
+  }
+
+  async findByEmail(email: string) {
     return await this.findOne({ where: { email } });
+  }
+
+  async createUser(user: User) {
+    const { id, name, email, password } = user;
+    return await this.save({ id, name, email, password });
+  }
+
+  async updateUser(user: User) {
+    const { id, name, email, password } = user;
+    await this.update({ id }, { name, email, password });
+    return user;
+  }
+
+  async deleteUser(id: string) {
+    await this.delete(id);
   }
 }
